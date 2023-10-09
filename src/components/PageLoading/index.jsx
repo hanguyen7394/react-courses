795491b385a0010299ddd8d6f31d977024fceb03
Ha Nguyen <ha.ntt@sutrixsolutions.com>
2023-10-09 19:25:13 +0700

    - Change Page Loading

--- src/components/PageLoading/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PageLoading = () => {
-  return (
-    <div className="loading" />
-  )
-}
-
-export default PageLoading
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,39 +1,28 @@
-import Footer from '../components/Footer';
-import ModalLogin from '../components/ModalLogin';
-import Navbar from '../components/Navbar';
-import Header from '../components/Header';
-import PageLoading from '../components/PageLoading';
+import { Suspense, lazy } from 'react';
+const Footer = lazy(() => import('../components/Footer'));
+const ModalLogin = lazy(() => import('../components/ModalLogin'));
+const Navbar = lazy(() => import('../components/Navbar'));
+const Header = lazy(() => import('../components/Header'));
+const Overlay = lazy(() => import('../components/Overlay'));
 import { Outlet } from 'react-router-dom';
-import Overlay from '../components/Overlay';
 import MainContextProvider from '../context/MainContext';
 import AuthContextProvider from '../context/AuthContext';
-import { useEffect, useState } from 'react';
 import ComponentLoading from '../components/ComponentLoading';
 
 const MainLayout = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(false);
-  }, []);
-
   return (
     <MainContextProvider>
       <AuthContextProvider>
-        {loading ? (
-          <ComponentLoading style={{ height: '100vh' }} />
-        ) : (
-          <>
-            <Header />
-            <Navbar />
-            <Overlay />
+        <Suspense fallback={<ComponentLoading style={{height: '100vh'}} />}>
+          <Header />
+          <Navbar />
+          <Overlay />
 
-            <Outlet />
+          <Outlet />
 
-            <Footer />
-            <ModalLogin />
-          </>
-        )}
+          <Footer />
+          <ModalLogin />
+        </Suspense>
       </AuthContextProvider>
     </MainContextProvider>
   );
